35ebc1d68373e36eee40540b7400a46f46573852
bohdan_kochetov <bodja1208@gmail.com>
2021-10-07 13:47:40 +0300

    Updates

---
+++ precache-manifest.05cd293b56fcb7d3c202fd502aad4f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df104c67fdf76a4041c0e555d0ca4e4b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e44898701abb7c226fe",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e44898701abb7c226fe",
+    "url": "./static/js/main.b4e68938.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
